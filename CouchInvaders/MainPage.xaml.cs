3740e0f602a2d164628b8979b74ce473c0ca6e0d
ChewBacher <chris.raubacher@gmail.com>
2020-10-03 19:50:01 -0700

    Add AnimatedControl, Basic RenderLoop

--- CouchInvaders/MainPage.xaml.cs
+++ CouchInvaders/MainPage.xaml.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.Brushes;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Numerics;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.System;
+using Windows.UI;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +31,84 @@ namespace CouchInvaders
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        // UI Thread
+        private void MainCanvas_CreateResources(Microsoft.Graphics.Canvas.UI.Xaml.CanvasAnimatedControl sender, Microsoft.Graphics.Canvas.UI.CanvasCreateResourcesEventArgs args)
+        {
+
+        }
+
+        // Game Thread
+        private void MainCanvas_Draw(Microsoft.Graphics.Canvas.UI.Xaml.ICanvasAnimatedControl sender, Microsoft.Graphics.Canvas.UI.Xaml.CanvasAnimatedDrawEventArgs args)
+        {
+            CanvasDrawingSession ds = args.DrawingSession;
+
+            Size rtSize = sender.Size;
+
+            // Background gradient.
+            using (var brush = CanvasRadialGradientBrush.CreateRainbow(ds, 0))
+            {
+                brush.Center = rtSize.ToVector2() / 2;
+
+                brush.RadiusX = (float)rtSize.Width;
+                brush.RadiusY = (float)rtSize.Height;
+
+                ds.FillRectangle(0, 0, (float)rtSize.Width, (float)rtSize.Height, brush);
+            }
+
+            // Text label.
+            var label = string.Format("{0}\n{1:0} x {2:0}", "Couch Invaders!!!", rtSize.Width, rtSize.Height);
+
+            ds.DrawText(label, rtSize.ToVector2() / 2, Colors.Black);
+
+        }
+
+        // Game Thread
+        private void MainCanvas_GameLoopStarting(Microsoft.Graphics.Canvas.UI.Xaml.ICanvasAnimatedControl sender, object args)
+        {
+
+        }
+
+        // Game Thread
+        private void MainCanvas_GameLoopStopped(Microsoft.Graphics.Canvas.UI.Xaml.ICanvasAnimatedControl sender, object args)
+        {
+
+        }
+
+        // Game Thread
+        private void MainCanvas_Update(Microsoft.Graphics.Canvas.UI.Xaml.ICanvasAnimatedControl sender, Microsoft.Graphics.Canvas.UI.Xaml.CanvasAnimatedUpdateEventArgs args)
+        {
+
+        }
+
+        // UI Thread
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            Window.Current.CoreWindow.KeyDown += KeyDown_UIThread;
+        }
+
+        // UI Thread
+        private void KeyDown_UIThread(CoreWindow sender, KeyEventArgs args)
+        {
+            args.Handled = true;
+
+            // extract the data from the args before marshaling it to the
+            // game loop thread
+            var virtualKey = args.VirtualKey;
+
+            var action = MainCanvas.RunOnGameLoopThreadAsync(() => KeyDown_GameLoopThread(virtualKey));
+        }
+
+        // UI Thread
+        private void Page_Unloaded(object sender, RoutedEventArgs e)
+        {
+            Window.Current.CoreWindow.KeyDown -= KeyDown_UIThread;
+        }
+
+        private void KeyDown_GameLoopThread(VirtualKey virtualKey)
+        {
+            // ...
         }
     }
 }
